90935a90676b5f161f43577686c2f0a951408bf0
Manuel Herrmann <it@icetruck.de>
2015-10-16 06:01:08 +0200

    reorganization

--- client.hs
+++ client.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE NamedFieldPuns, DeriveDataTypeable, ExtendedDefaultRules, EmptyDataDecls, MultiParamTypeClasses #-}
 module Main where
 import Control.Concurrent (forkIO, killThread)
+import qualified Control.Concurrent.Thread as Thread (forkIO, forkOS, result)
 import Control.Concurrent.Thread.Delay (delay)
 import Control.Concurrent.MVar
 import Control.Monad (forever, unless, when)
@@ -10,6 +11,7 @@ import Control.Exception.Base (try, SomeException)
 import Network.Socket (withSocketsDo)
 import Data.Text (Text)
 import Data.Text.Lazy (toStrict)
+import System.IO.Unsafe (unsafePerformIO)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Network.WebSockets as WS
@@ -41,15 +43,6 @@ cmdJoin channel nick = do
     toStrict $ E.decodeUtf8 $ J.encode $ J.object [ T.pack "cmd" .= "join", T.pack "channel" .= channel, T.pack "nick" .= nick ]
 
 
-data Chat = Chat { chatWindow :: Window, chatExitCode :: Int }
-
-
-newtype MyCurses a = MyCurses { myCursesIO :: IO a }
-unCurse :: N.Curses a -> IO a
-unCurse x = myCursesUnpack (unsafeCoerce x :: MyCurses a)
-    where
-    myCursesUnpack (MyCurses a) = a
-
 
 waitFor :: Window -> (N.Event -> Bool) -> Curses ()
 waitFor w p = loop where
@@ -60,44 +53,24 @@ waitFor w p = loop where
             Just ev' -> if p ev' then return () else loop
 
 
-app :: MVar Chat -> WS.ClientApp()
-app state conn = do
-    c <- takeMVar state :: IO Chat
-    unCurse $ do
-        N.updateWindow (chatWindow c) $ do
-            moveCursor 1 10
-            drawString "Connected!"
-            moveCursor 0 0
-            drawBox Nothing Nothing
-        N.render
 
+app :: MVar Bool -> WS.ClientApp()
+app stopping conn = do
     let joinCmd = cmdJoin cfg_channel cfg_nick
     WS.sendTextData conn $ joinCmd
 
     recvThreadId <- forkIO $ forever $ do
-        msg <- WS.receiveData conn
-        liftIO $ unCurse $ do
-            N.updateWindow (chatWindow c) $ do
-                moveCursor 1 10
-                drawString $ T.unpack msg
-                moveCursor 0 0
-                drawBox Nothing Nothing
-            N.render
+        _ <- return.toStrict =<< WS.receiveData conn
+        return $! ()
 
     pingThreadId <- forkIO $ forever $ do
         delay (60*1000*1000) >> WS.sendTextData conn (cmdPing)
 
     let loop = do
-        ev <- unCurse (getEvent (chatWindow c) (Just 300))
-        let quit = case ev of
-                        Just e -> e == EventCharacter 'q' || e == EventCharacter 'Q'
-                        Nothing -> False
-
-        if quit then do
-            let c2 = c {chatExitCode = 0}
-            putMVar state c2
-            return False -- No auto restart
-        else loop
+        stopstatus <- readMVar stopping
+        case stopstatus of
+            True  -> return ()
+            False -> delay (300*1000) >> loop
     loop
 
     killThread recvThreadId
@@ -107,36 +80,70 @@ app state conn = do
 
 
 
-runClient chat = do
-    result <- try (withSocketsDo $ WS.runClient cfg_server cfg_port "/chat-ws" (app chat)) :: IO (Either SomeException ())
-    -- ignore result for now
-    c <- readMVar chat
-    if (chatExitCode c) /= -1 then do
-        return True
-    else runClient chat
+
+runGui :: MVar Bool -> IO()
+runGui clientStopping = do
+    runCurses $ do
+        window <- defaultWindow
+        oldCursorMode <- setCursorMode CursorInvisible
+        
+        N.updateWindow window $ do
+            moveCursor 1 10
+            drawString "Hello World!"
+            moveCursor 3 10
+            drawString "(press q to quit)"
+            moveCursor 0 0
+            drawBox Nothing Nothing
+        N.render
+        
+        -- TODO: Render chat and updates here
+{-
+
+        liftIO $ unCurse $ do
+            N.updateWindow (chatWindow c) $ do
+                moveCursor 1 10
+                drawString $ T.unpack msg
+                moveCursor 0 0
+                drawBox Nothing Nothing
+            N.render
+
+    unCurse $ do
+        N.updateWindow (chatWindow c) $ do
+            moveCursor 1 10
+            drawString "Connected!"
+            moveCursor 0 0
+            drawBox Nothing Nothing
+        N.render
+-}
+        
+        let loop = do
+            ev <- getEvent window (Just 300)
+            let quit = case ev of
+                            Just e -> e == EventCharacter 'q' || e == EventCharacter 'Q'
+                            Nothing -> False
+
+            if quit then do
+                liftIO $ takeMVar clientStopping >> putMVar clientStopping True >> return ()
+            else loop
+        loop
+        
+        _ <- setCursorMode oldCursorMode
+        return ()
+
+
+
+runNet :: MVar Bool -> IO()
+runNet stopping = do
+    withSocketsDo $ WS.runClient cfg_server cfg_port "/chat-ws" (app stopping)
 
 
 
 main :: IO()
-main = runCurses $ do
-    window <- defaultWindow
-    oldCursorMode <- setCursorMode CursorInvisible
+main = do
+    clientStopping <- newMVar False
 
-    chat <- liftIO $ newMVar $ Chat { chatWindow = window, chatExitCode = -1 }
-    c <- liftIO $ readMVar chat
+    (_, guiWait) <- Thread.forkOS $ runGui clientStopping
+    (_, netWait) <- Thread.forkOS $ runNet clientStopping
 
-    N.updateWindow (chatWindow c) $ do
-        moveCursor 1 10
-        drawString "Hello World!"
-        moveCursor 3 10
-        drawString "(press q to quit)"
-        moveCursor 0 0
-        drawBox Nothing Nothing
-    N.render
-
-    liftIO $ runClient chat
-
-    _ <- setCursorMode oldCursorMode
-
-    liftIO $ putStrLn "End"
+    netWait >> guiWait >> (return $! ())
 
