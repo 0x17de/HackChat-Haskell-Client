b69ac746153c22a3d0c903656b971dc06fde7bbd
Manuel Herrmann <it@icetruck.de>
2015-10-17 08:08:52 +0200

    parallel threads working

--- client.hs
+++ client.hs
@@ -1,46 +1,66 @@
 {-# LANGUAGE NamedFieldPuns, DeriveDataTypeable, ExtendedDefaultRules, EmptyDataDecls, MultiParamTypeClasses #-}
 module Main where
-import Control.Concurrent (forkIO, killThread)
+import System.IO (hPutStrLn, stderr)
+import System.Posix.Signals as SIG
+import Control.Applicative ((<|>))
+import Control.Concurrent (forkIO, forkOS, killThread)
 import qualified Control.Concurrent.Thread as Thread (forkIO, forkOS, result)
 import Control.Concurrent.Thread.Delay (delay)
 import Control.Concurrent.MVar
 import Control.Monad (forever, unless, when)
 import Control.Monad.Trans (liftIO)
 import Control.Monad.State (modify)
-import Control.Exception.Base (try, SomeException)
+import Control.Exception.Base (mask, onException, catch, SomeException)
 import Network.Socket (withSocketsDo)
 import Data.Text (Text)
 import Data.Text.Lazy (toStrict)
-import System.IO.Unsafe (unsafePerformIO)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Network.WebSockets as WS
+import qualified Foreign.C.Types as C
 
 import Data.Aeson as J
 import Data.Text.Lazy.Encoding as E
 
 import qualified UI.HSCurses.Curses as N
-import Unsafe.Coerce
+import qualified UI.HSCurses.Widgets as NW
 
 
 
 cfg_server = "toastystoemp.com"
 cfg_port = 6060
-cfg_channel = "programming"
+cfg_channel = "testing"
 cfg_nick = "HaskellNick"
 
 
 
-newEventQueue :: IO (MVar [a])
-newEventQueue = newMVar []
-addEvent :: (MVar [a]) -> a -> IO()
-addEvent q e = do
-    takeMVar q >>= \x -> putMVar q (x++[e]) >> return ()
-takeEvent :: MVar [a] -> IO a
+data Event = EventButton C.CInt | EventNop
+data Queue = Queue (MVar Bool) (MVar [Event])
+
+
+newEventQueue :: IO Queue
+newEventQueue = do
+    lock <- newEmptyMVar
+    queue <- newMVar []
+    return $ Queue lock queue
+addEvent :: Queue -> Event -> IO()
+addEvent (Queue lock queue) e = do
+    a <- takeMVar queue
+    success <- tryPutMVar lock True
+    putMVar queue (a++[e])
+    return ()
+    
+takeEvent :: Queue -> IO Event
 takeEvent q = do
-    (x:xs) <- takeMVar q
-    putMVar q xs
-    return x
+    let (Queue lock queue) = q
+    a <- takeMVar queue
+    case a of
+        [] -> do
+            putMVar queue []
+            tryTakeMVar lock
+            tryReadMVar lock
+            takeEvent q
+        otherwise -> putMVar queue (tail a) >> return (head a)
 
 
 
@@ -57,20 +77,20 @@ cmdJoin channel nick = do
 
 
 
-app :: MVar Bool -> MVar [a] -> WS.ClientApp()
-app stopping guiEvents conn = do
+app :: MVar Bool -> Queue -> Queue -> WS.ClientApp()
+app clientStopping guiEvents netEvents conn = do
     WS.sendTextData conn $ cmdJoin cfg_channel cfg_nick
 
     recvThreadId <- forkIO $ forever $ do
-        _ <- return.toStrict =<< WS.receiveData conn
+        putStrLn.T.unpack.toStrict =<< WS.receiveData conn
         return $! ()
 
     pingThreadId <- forkIO $ forever $ do
         delay (60*1000*1000) >> WS.sendTextData conn (cmdPing)
 
     let loop = do
-        stopstatus <- readMVar stopping
-        case stopstatus of
+        stopping <- readMVar clientStopping
+        case stopping of
             True  -> return ()
             False -> delay (300*1000) >> loop
     loop
@@ -82,34 +102,76 @@ app stopping guiEvents conn = do
 
 
 
-
-runGui :: MVar Bool -> MVar [a] -> IO()
-runGui clientStopping netEvents = do
+runGui :: MVar Bool -> Queue -> Queue -> IO()
+runGui clientStopping guiEvents netEvents = do
     window <- N.initScr
     oldCursorMode <- N.cursSet N.CursorInvisible
+    N.echo False
     
-    -- TODO: Render chat and updates here
+    -- HANDLER
+    let sigWinchHandler signalInfo = do
+        case (siginfoSignal signalInfo) of
+            siginfoSignal -> do
+                hPutStrLn stderr "Resize"
+    
+    case N.cursesSigWinch of
+        Just a -> installHandler (a) (SIG.CatchInfo $ sigWinchHandler) Nothing
+    
+    -- INITIALIZE VIEWS
+    --let tableWidet = NW.newTableWidget (NW.TBWOptions {}) []
+    
+    -- REFRESH
+    --N.wRefresh window
+    
+    -- LOOP
+    -- keyboard events
+    keyboardThread <- forkIO $ forever $ do
+        hPutStrLn stderr $ "Key"
+        keyCode <- N.getch
+        hPutStrLn stderr $ "KeyCode " ++ (show keyCode)
+        addEvent guiEvents (EventButton keyCode)
+        return ()
 
-    let loop = do
-        key <- N.getCh
-        case key of
-            N.KeyCancel -> liftIO $ takeMVar clientStopping >> putMVar clientStopping True
-        
-        stopping <- readMVar clientStopping
-        case stopping of
-            True  -> return ()
-            False -> loop
-    loop
-    
-    _ <- N.cursSet oldCursorMode
+    -- process keyboard events
+    let processButtonAction :: C.CInt -> IO()
+        processButtonAction keyCode = do
+            liftIO $ hPutStrLn stderr $ "Keycode " ++ (show keyCode)
+            when (keyCode == 27) $ liftIO $ takeMVar clientStopping >> putMVar clientStopping True
+            return ()
+
+    -- main event loop
+    let eventLoop :: IO()
+        eventLoop = do
+            hPutStrLn stderr "LOOP"
+            stopping <- readMVar clientStopping
+            unless(stopping) $ do
+                hPutStrLn stderr "UNLESS"
+                e <- takeEvent guiEvents
+                hPutStrLn stderr "UNLESS_1"
+                case e of
+                    EventButton button -> processButtonAction button
+                    EventNop -> return ()
+                eventLoop
+
+    -- SPAWN
+    (_, waitEvents) <- Thread.forkIO $ eventLoop
+
+    -- WAIT & EXIT
+    waitEvents
+    killThread keyboardThread
+    N.cursSet oldCursorMode
+    N.echo True
     N.endWin
-    return ()
 
 
 
-runNet :: MVar Bool -> MVar [a] -> IO()
-runNet stopping guiEvents = do
-    withSocketsDo $ WS.runClient cfg_server cfg_port "/chat-ws" (app stopping guiEvents)
+runNet :: MVar Bool -> Queue -> Queue -> IO()
+runNet stopping guiEvents netEvents = do
+    catch (withSocketsDo $ WS.runClient cfg_server cfg_port "/chat-ws" (app stopping guiEvents netEvents))
+        (\e -> do
+                    let err = show (e :: SomeException)
+                    hPutStrLn stderr ("Error: " ++ err)
+                    return ())
 
 
 
@@ -120,8 +182,10 @@ main = do
     guiEvents <- newEventQueue
     netEvents <- newEventQueue
 
-    (_, guiWait) <- Thread.forkOS $ runGui clientStopping netEvents
-    (_, netWait) <- Thread.forkOS $ runNet clientStopping guiEvents
+    (_, netWait) <- Thread.forkIO $ runNet clientStopping guiEvents netEvents
+    (_, guiWait) <- Thread.forkIO $ runGui clientStopping guiEvents netEvents
 
-    netWait >> guiWait >> (return $! ())
+    netWait
+    guiWait
+    return $! ()
 
